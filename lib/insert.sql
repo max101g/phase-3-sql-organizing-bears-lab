6051c8ba7cd92db533c69555b6e4075f068745ba
max101g <maxgithinji101@gmail.com>
2023-02-20 09:23:25 +0300

    Ruby commit

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES ("Mr.Chocolate", 20, "M", "dark brown", "calm", 0);
+       ("Rowdy", 10, "M", "black", "intense", 1);
+       ("Tabitha", 6, "F", "dark brown", "Nice", 1);
+       ("Sergeant Brown", 19, "M", "Green", "Slimy", 0);
+       ("Melissa", 13, "F", "dark brown", "goofy", 1);
+       ("Grinch", 2, "M", "Black", "Grinchy", 1);
+       ("Wendy", 6, "F", "blue", "naive", 1);
+       (null, 20, "M", "Chestnut", "Joyful", 0);
+       
